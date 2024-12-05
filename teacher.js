39d136d383ef965493d212698f4ef6ce0a1bc26c
BlackCode <156558073+shoaib-codeHub@users.noreply.github.com>
2024-12-05 15:05:21 +0530

    made faculty page successfully

---
+++ teacher.js
@@ -0,0 +1,1 @@
+document.addEventListener("DOMContentLoaded", function() { var contentElement = document.getElementById('course-content'); setTimeout(function() { contentElement.classList.add('show'); }, 100); });
